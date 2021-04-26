76999321caea7c14128cb0bba722cb7ee2978345
Simone Bergonzi <73989372+MurasakiSimema@users.noreply.github.com>
2021-04-26 16:21:58 +0200

    Add files via upload

---
+++ proxy.php
@@ -0,0 +1,16 @@
+<?php
+$url = "http://extra.isii.it:40081/donatori/paypal/payments.php";
+$ch = curl_init( $url );
+# Setup request to send json via POST.
+file_put_contents("test.txt", json_encode($_POST));
+$payload = json_encode( $_POST );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+# Return response instead of printing.
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+# Send request.
+$result = curl_exec($ch);
+curl_close($ch);
+# Print response.
+echo "<pre>$result</pre>";
+?>
