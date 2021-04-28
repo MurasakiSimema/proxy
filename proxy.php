f9c3a92bec29fe433618a31f8f15f4d168f3705e
Simone Bergonzi <73989372+MurasakiSimema@users.noreply.github.com>
2021-04-28 09:22:09 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -3,7 +3,7 @@ $url = "http://extra.isii.it:40081/donatori/paypal/payments.php";
 $ch = curl_init( $url );
 # Setup request to send json via POST.
 file_put_contents("test.txt", json_encode($_POST));
-$payload = $_POST;
+$payload = json_encode($_POST);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
 curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
